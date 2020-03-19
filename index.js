2b374a048637f63097360f3a01b814109f8aef77
Emily Déat <emily.deat@gmail.com>
2020-03-19 15:49:55 +0100

    Add dark mode on all page

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
-const darkMode = document.getElementById("dark-mode")
+const darkMode = document.getElementById('dark-mode')
 
 darkMode.onclick = function() {
-    if (document.documentElement.hasAttribute('data-theme', 'dark')) {
-        document.documentElement.removeAttribute('data-theme', 'dark')
-        console.log("ok 2")
+    if (document.documentElement.getAttribute('data-theme') === 'dark') {
+        // White Mode
+        document.documentElement.setAttribute('data-theme', 'ligth')
     } else {
+        // Dark Mode
         document.documentElement.setAttribute('data-theme', 'dark')
-        console.log("ok")
     }
+
+    sessionStorage.setItem('dataTheme', document.documentElement.getAttribute('data-theme'))
+}
+
+const savedDataTheme = sessionStorage.getItem('dataTheme')
+
+switch (savedDataTheme) {
+    case 'dark':
+        document.documentElement.setAttribute('data-theme', 'dark')
+        break
+    case 'light':
+        document.documentElement.setAttribute('data-theme', 'ligth')
+        break
+    default:
+        sessionStorage.setItem('dataTheme', 'light')
+        document.documentElement.setAttribute('data-theme', 'ligth')
+        break
 }
